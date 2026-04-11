e510d66b6cdcd847de3575eb8a4e7fe08f5b6dfe
mtrznadel24 <maciej.trznadel24@gmail.com>
2026-04-12 00:16:13 +0200

    feat(auth): add password to User and initial JWT filter boilerplate

--- backend/project-manager/src/main/java/pl/edu/agh/project_manager/domain/entity/User.java
+++ backend/project-manager/src/main/java/pl/edu/agh/project_manager/domain/entity/User.java
@@ -34,6 +34,9 @@ public class User {
     @Column(name = "surname")
     private String surname;
 
+    @Column(name = "password")
+    private String password;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "user_role", nullable = false)
     private UserRole userRole;
