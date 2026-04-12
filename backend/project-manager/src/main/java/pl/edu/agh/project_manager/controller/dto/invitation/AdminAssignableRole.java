72008eb581770d0046b76f59107b4fa849c5da75
radek <radek.rogalski@gmail.com>
2026-04-12 21:49:11 +0200

    Cleanup

--- backend/project-manager/src/main/java/pl/edu/agh/project_manager/controller/dto/invitation/AdminAssignableRole.java
+++ backend/project-manager/src/main/java/pl/edu/agh/project_manager/controller/dto/invitation/AdminAssignableRole.java
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:backend/project-manager/src/main/java/pl/edu/agh/project_manager/controller/dto/invitation/AdminAssignableRole.java
 package pl.edu.agh.project_manager.controller.dto.invitation;
+========
+package pl.edu.agh.project_manager.controller.dto.user;
+>>>>>>>> 92cad07 (Cleanup):backend/project-manager/src/main/java/pl/edu/agh/project_manager/controller/dto/user/AdminAssignableRole.java
 
 public enum AdminAssignableRole {
     COMMON,
