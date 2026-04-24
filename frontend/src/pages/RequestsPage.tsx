94cc300faf29ffff54d1828767aae10364074361
radek <radek.rogalski@gmail.com>
2026-04-25 00:23:07 +0200

    cleanup

---
+++ frontend/src/pages/RequestsPage.tsx
@@ -0,0 +1,10 @@
+
+export const RequestsPage = () => {
+  return (
+    <div className="flex items-center justify-center p-4">
+      <div className="w-full max-w-md">
+        <p>Requests Page</p>
+      </div>
+    </div>
+  );
+};
