c75c218b699588057102a88943e84d3024678a93
Jan Dyląg <jan.dylag@gmail.com>
2026-05-11 21:02:22 +0200

    naprawa przypisania menedżerów liniowych + możliwość wyboru menedżera (#51)
    naprawa przypisania menedżerów liniowych + możliwość wyboru menedżera przez admina podczas tworzenia konta pracownika

---
+++ backend/project-manager/src/main/java/pl/edu/agh/project_manager/controller/user/UserSearchableRole.java
@@ -0,0 +1,9 @@
+package pl.edu.agh.project_manager.controller.user;
+
+public enum UserSearchableRole {
+    COMMON,
+    AUTHORITY,
+    LINEAR_MANAGER,
+    PROJECT_MANAGER,
+    ADMINISTRATOR,
+}
