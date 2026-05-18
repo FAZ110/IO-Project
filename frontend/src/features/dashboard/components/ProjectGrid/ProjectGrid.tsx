ab41c6beeb536d07eda5461d65a9e70fb901b831
PatrykBlacha <pblacha@student.agh.edu.pl>
2026-05-19 01:01:54 +0200

    Minor styles change

--- frontend/src/features/dashboard/components/ProjectGrid/ProjectGrid.tsx
+++ frontend/src/features/dashboard/components/ProjectGrid/ProjectGrid.tsx
@@ -17,7 +17,7 @@ export const ProjectGrid = ({ projects }: ProjectGridProps) => {
     }
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6 pt-4">
             {projects.map((project) => (
                 <Link
                     key={project.id}
