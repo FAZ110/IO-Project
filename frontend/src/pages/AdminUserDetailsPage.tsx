08445a59d6057bd407d9491546e01840c9d60038
Szymon Barczyk <szymonbarczyk@icloud.com>
2026-05-25 22:49:37 +0200

    frontend: render profile sections dynamically based on user role

--- frontend/src/pages/AdminUserDetailsPage.tsx
+++ frontend/src/pages/AdminUserDetailsPage.tsx
@@ -1,15 +1,3 @@
-import { useParams } from 'react-router-dom';
-import { ROUTE_PARAMS } from '@/routes/paths';
+import { EmployeeDetailsPage } from './EmployeeDetailsPage';
 
-export const AdminUserDetailsPage = () => {
-  const { [ROUTE_PARAMS.USER_ID]: userId } = useParams();
-
-  return (
-    <div className="p-4 sm:p-6">
-      <div className="max-w-4xl mx-auto">
-        <h1 className="text-2xl font-bold text-gray-800">Profil użytkownika</h1>
-        <p className="mt-2 text-gray-500">TODO: szczegóły użytkownika {userId}</p>
-      </div>
-    </div>
-  );
-};
+export const AdminUserDetailsPage = () => <EmployeeDetailsPage />;
